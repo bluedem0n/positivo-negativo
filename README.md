e790fffe823313377d0a0eb6511d2695c9f23132
Sthefany Floriano <sthefanyflorianog@gmail.com>
2016-08-26 12:34:50 -0500

    Subiendo repo

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # LS17-EJERCICIO1
+
+Es positivo o negativo?
+1. Inicio
+2. Leer Datos
+* dato 1
+* dato 2
+3. Sumar datos
+4. Entregar suma
+5. Fin
+
+![](http://i66.tinypic.com/262rzoh.jpg)
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var num = parseInt(prompt("Ingresa un número"));
-if (num<0) {
-    alert("El número es negativo")
-} else {
+if (num>0) {
     alert("El número es positivo")
+} else if (num<0){
+    alert("El número es negativo")
+}
+else {
+    alert("El numero es cero")
 }
